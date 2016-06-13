12206253c7a88bcd4620b60b0a18968e17ae4599
joshwarren <joshua.warren@physics.ox.ac.uk>
2016-06-13 18:50:00 +0100

    EoDC: Update to reduce.sh
     On branch master
     Changes to be committed:
    	modified:   reduce.sh

--- reduce.sh
+++ reduce.sh
@@ -12,21 +12,24 @@ END
 }
 
 
-
-cd
-pargal=*
+pargal=NGC3100
+ob=1
 
 bias=false
 setup=false
-lamp=false
+lamp=true
 reduce=false
 skysub=false
+dar=false
+combine=false
 cube=false
-dar=true
+scp=false
 
+current=$( pwd )
+cd
 
 if [ "$bias" = true ] ; then
-    for dir in $( ls -d vimos/*/bias ) ; do
+    for dir in $( ls -d vimos/$pargal/$ob/bias ) ; do
         cd $dir
         mkdir ../temp/
         night=$( find VIMOS_SPEC_BIAS*.fits | awk -F'_' '$3 ~ /BIAS/ {print $3}' | head -n1 | sed s/BIAS// )
@@ -37,7 +40,7 @@ if [ "$bias" = true ] ; then
 fi
 
 
-for dir in $( ls -d vimos/$pargal/[1-3]/ ) ; do
+for dir in $( ls -d vimos/$pargal/$ob/ ) ; do
     cd $dir
     if [ "$setup" = true ] ; then
         Py3D vimos renameFiles 2012
@@ -56,28 +59,75 @@ for dir in $( ls -d vimos/$pargal/[1-3]/ ) ; do
         gal=$( readheader VIMOS_IFU_OBS$night"_"$num"_"A.2.fits.gz | awk -F'OBJECT' '{print $2}' | awk -F\' '{print $2}' | sed 's/ //' | sed 's/ //' )
         if [ "$reduce" = true ] ; then
             Py3D vimos reduceObjectHR $night $gal"_"$num $num wave_start=4000 wave_end=5340 wave_disp=0.7 res_fwhm=2.5 setup=blue fiberflat=1 telluric_cor=0
-            mkdir ../../$gal
-            cp $gal"_"$num.fobj.fits ../../$gal"_"$night"_"$num.fobj.fits
         fi
         if [ "$skysub" = true ] ; then
-            echo $gal"_"$num.fobj.fits
             Py3D rss splitFibers $gal"_"$num.fobj.fits $gal"_"$num"_"Q1.fits,$gal"_"$num"_"Q2.fits,$gal"_"$num"_"Q3.fits,$gal"_"$num"_"Q4.fits QD1,QD2,QD3,QD4
             for Q in $( seq 1 4 ) ; do
                  Py3D rss constructSkySpec  $gal"_"$num"_"Q$Q.fits SKY_Q$Q"_"$num.fits nsky=60
                  Py3D rss subtractSkySpec $gal"_"$num"_"Q$Q.fits $gal"_"$num"_"skys_Q$Q.fits SKY_Q$Q"_"$num.fits
             done
-        fi
-        if [ "$cube" = true ] ; then
-            Py3D rss mergeRSS $gal"_"$num"_"skys_Q1.fits,$gal"_"$num"_"skys_Q2.fits,$gal"_"$num"_"skys_Q3.fits,$gal"_"$num"_"skys_Q4.fits $gal"_"$num.sobj.fits mergeHdr=1
-            Py3D rss createCube $gal"_"$num.sobj.fits $gal"_"$num.sobj.cube.fits mode='drizzle' resolution=0.67
+            Py3D rss mergeRSS $gal"_"$num"_"skys_Q1.fits,$gal"_"$num"_"skys_Q2.fits,$gal"_"$num"_"skys_Q3.fits,$gal"_"$num"_"skys_Q4.fits $gal"_"$night"_"$num.sobj.fits mergeHdr=1
         fi
         if [ "$dar" = true ] ; then
-            Py3D cube measureDARPeak $gal"_"$num.sobj.cube.fits DAR_$gal"_"$num coadd=20  steps=20 fibers=1400 figure_out=$gal"_"$num"_"DAR_X.png
+
+            Py3D rss createCube $gal"_"$night"_"$num.sobj.fits $gal"_"$night"_"$num.sobj.cube.fits mode='drizzle' resolution=0.67
+
+            Py3D cube measureDARPeak $gal"_"$night"_"$num.sobj.cube.fits DAR_$gal"_"$night"_"$num coadd=20  steps=20 fibers=1600 figure_out=$gal"_"$num"_"DAR_X.png
         fi
 
 
 
     done
+    cd ../..
+    if [ "$combine" = true ] ; then
+        cp $( ls [1-3]/temp/*.sobj.fits ) ./
+        cp $( ls [1-3]/temp/*.rss.cent_x.fits ) ./
+        cp $( ls [1-3]/temp/*.rss.cent_y.fits ) ./
 
+        numbers=$( ls *.sobj.fits | awk -F'[_.]' '{print $2}' )
+        srss=""
+        sxdar=""
+        sydar=""
+	for f in $( ls *.sobj.fits ) ; do
+	    srss=$srss","$f
+	done
+	for f in $( ls *.rss.cent_x.fits ) ; do
+	    sxdar=$sxdar","$f
+	done
+	for f in $( ls *.rss.cent_y.fits ) ; do
+	    sydar=$sydar","$f
+	done
+
+        srss=$( echo $srss | cut -c 2- )
+        sxdar=$( echo $sxdar | cut -c 2- )
+        sydar=$( echo $sydar | cut -c 2- )
+ 
+        Py3D rss mergeRSS $srss $gal"_"COMBINED_RSS.fits mergeHdr=0
+        Py3D rss mergeRSS $sxdar $gal"_"COMBINED.cent_x.fits mergeHdr=0
+        Py3D rss mergeRSS $sydar $gal"_"COMBINED.cent_y.fits mergeHdr=0
+
+
+       Py3D rss createCube $gal"_"COMBINED_RSS.fits "${gal,,}".cube.combined.fits position_x=$gal"_"COMBINED.cent_x.fits position_y=$gal"_"COMBINED.cent_y.fits ref_pos_wave=5000 mode='drizzle' resolution=0.67 full_field=1
+    fi
+
+
+    if [ "$cube" = true ] ; then
+       cp "${gal,,}".cube.combined.fits ../../cubes/
+    fi
+
+    if [ "$scp" = true ] ; then
+        scp "${gal,,}".cube.combined.fits warrenj@asosx146.nat.physics.ox.ac.uk:/Data/vimos/$gal/
+    fi
     cd
 done
+
+
+
+
+
+
+
+
+
+
+cd $current
